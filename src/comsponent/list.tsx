b1e4afeeb8f5d41575c58b945beadcfb0217b229
m83city <rostorguievv@gmail.com>
2022-06-06 10:38:20 +0300

    ignore

---
+++ src/comsponent/list.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import {Select} from "antd"
+
+
+function App() {
+const [language, setLanguage] =useState();
+
+
+
+  return (
+    <div>
+        
+
+    </div>
+  );
+}
+
+export default App;
