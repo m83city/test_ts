a74a3ee2f752f8957adbba43ca8b274de42d3032
m83 <rostorguievv@gmail.com>
2023-04-14 10:25:21 +0300

    google Oauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useRef } from 'react';
+import React, { SyntheticEvent, useRef, useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google'
+import ApiCalendar from "react-google-calendar-api";
+import { sign } from 'crypto';
+
+
 
 function App() {
-   const inputEl = useRef(null);
-  const onButtonClick = () => {
+  const [clID, setClID] = useState('');
 
+
+
+  const responseMessage = (response: any) => {
+    console.log(response);
+    setClID(response.clientId)
+    return response
+  }
+  const errorMessage = () => {
+    console.log('error');
+  }
+  const config = {
+    clientId: '852666352240-5tjdjq9bu6ikao8s2jnru023d5u5qjhi.apps.googleusercontent.com',
+    apiKey: "AIzaSyBARflBn10Og_e-dbUlJwXujVmt1HbCbFA",
+    scope: "https://www.googleapis.com/auth/calendar",
+    discoveryDocs: [
+      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    ],
   };
+  const apiCalendar = new ApiCalendar(config);
+  const handleItemClick = (event: SyntheticEvent<any>, name: string) => {
+    if (name === 'sign-in') {
+      apiCalendar.handleAuthClick()
+      //apiCalendar.handleAuthClick()
+    } else if (name === 'sign-out') {
+      apiCalendar.handleSignoutClick();
+    }
+  }
 
   return (
     <>
-      <input ref={inputEl} type="text" />
-      <button onClick={onButtonClick}>Фокусуватись на полі вводу</button>
+      <h1>React Google Login</h1>
+      <br />
+      <br />
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+      <button onClick={(e) => { handleItemClick(e, "sign-in") }} >logCALENDAR</button>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId='852666352240-5tjdjq9bu6ikao8s2jnru023d5u5qjhi.apps.googleusercontent.com'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
